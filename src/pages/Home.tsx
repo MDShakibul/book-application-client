de9e5af627ca27b8891bf3a7fb20168a4cfbfd11
MDShakibul <aitldev14@gmail.com>
2023-07-15 22:59:26 +0600

    home page book card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
 import { Button } from '@/components/ui/button';
 import banner from '@/assets/images/banner.png';
-import hero from '@/assets/images/hero.png';
 import { Link } from 'react-router-dom';
 import Footer from '@/layouts/Footer';
+import { useEffect, useState } from 'react';
+import { IProduct } from '@/types/globalTypes';
+import BookCard from '@/components/BookCard';
 
 export default function Home() {
+  const [data, setData] = useState<IProduct[]>([]);
+  useEffect(() => {
+    fetch('./data.json')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <>
       <div className="flex justify-between items-center h-[calc(100vh-80px)] max-w-7xl mx-auto ">
@@ -26,18 +35,17 @@ export default function Home() {
         </div>
       </div>
       <div className="mb-96">
-        <h1 className="text-6xl font-black text-center text-primary mb-2">
+        <h1 className="text-5xl font-black text-primary text-center uppercase mt-10 mb-10">
           Latest Books
         </h1>
-        <div>
-          <img className="mx-auto" src={hero} alt="" />
+        <div className="container grid grid-cols-4 gap-12 pb-20">
+          {data?.slice(0, 10).map((product) => (
+            <BookCard product={product} />
+          ))}
         </div>
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-5xl font-black text-primary uppercase mt-10">
-            The future of tech is here
-          </h1>
           <Button className="mt-10" asChild>
-            <Link to="/products">Brows all products</Link>
+            <Link to="/books">All Books</Link>
           </Button>
         </div>
       </div>
